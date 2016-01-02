ae4b41deea28d612d05b3d6c12de50c59121e6ed
MatthiasW <MatthiasWagner@zoho.com>
2016-01-02 19:19:23 +0100

    added IDs to Protocol
    added ID trasmission
    added Version check for Receiver

--- main.c
+++ main.c
@@ -9,7 +9,8 @@ mrfiPacket_t packetreceived;
 
 
 void main (void)
-{	WDTCTL = WDTPW+WDTHOLD;                   // Stop watchdog timer
+{
+	WDTCTL = WDTPW+WDTHOLD;                   // Stop watchdog timer
 	P1OUT  = BIT0;				// P1.0 input+pullup
     P1REN |= BIT0;				// P1.0 input+pullup
     P1IE  |= BIT0;				// P1.0 interrupt enable
@@ -44,23 +45,35 @@ int8_t RSSI_calculate(uint8_t rawValue)
 
 void MRFI_RxCompleteISR_new()	// in Components/mrfi/radios/family5/mrfi_radio.c
 {	// Data back form Sensor
+
+    // Protocoll
+    //   | ID | CMD | val1 |val2 | \n |
+    //   | 2B | 1B  |  4B  | 4B  | 1B |
+    //   9    11    12     16     20
+    //   0    2     3      7      11
 	MRFI_Receive(&packetreceived);
 
 	int offset = 9;
 	int i;
 
 	// Send data back to PC
-	if (packetreceived.frame[offset] == CMD_RSSI){
+	if (packetreceived.frame[offset +2] == CMD_RSSI){
 		int rssi = abs(RSSI_calculate(packetreceived.rxMetrics[0]));
 		//format the RSSI value
-		packetreceived.frame[10] = ' ';
-		packetreceived.frame[11] = (rssi < 100) ? ' ' : (rssi/100) +'0';
-		packetreceived.frame[12] = (rssi < 10) ? ' ' : (rssi/10)%10 +'0';
-		packetreceived.frame[13] = rssi%10 +'0';
-
+		packetreceived.frame[12] = ' ';
+		packetreceived.frame[13] = (rssi < 100) ? ' ' : (rssi/100) +'0';
+		packetreceived.frame[14] = (rssi < 10) ? ' ' : (rssi/10)%10 +'0';
+		packetreceived.frame[15] = rssi%10 +'0';
 	}
+	else if(packetreceived.frame[offset +2] == CMD_VERS){
+			packetreceived.frame[offset +7] = '1';
+			packetreceived.frame[offset +8] += packetreceived.frame[offset +8] == ' ' ? 0x10 : 0;
+			packetreceived.frame[offset +9] += packetreceived.frame[offset +9] == ' ' ? 0x10 : 0;
 
-	for(i=offset; i < packetreceived.frame[0]; i++)
+
+		}
+
+	for(i=offset; i <= packetreceived.frame[0]; i++)
 		printf("%c",packetreceived.frame[i]);
 	printf("\n");
 
@@ -68,15 +81,32 @@ void MRFI_RxCompleteISR_new()	// in Components/mrfi/radios/family5/mrfi_radio.c
 
 #pragma vector=USCI_A0_VECTOR
 __interrupt void USCI_A0_ISR(void)
-{	//Receive Data from PC UART
-
+{	//Receive Data from PC UART and send via MRFI
+	static int count = 0;
+	int offset = 9;
 	char in_key = UCA0RXBUF;
-	packet.frame[0] = 11;
-	packet.frame[9] = in_key;
-	packet.frame[10] = 0x00;
-	packet.frame[11] = 0x00;
 
-	while(MRFI_TX_RESULT_SUCCESS!=MRFI_Transmit(&packet, MRFI_TX_TYPE_FORCED));
+	if(in_key == 0x0A) // '\n'
+	{
+		while(MRFI_TX_RESULT_SUCCESS!=MRFI_Transmit(&packet, MRFI_TX_TYPE_FORCED));
+		count = 0;
+	}
+	else if(in_key == CMD_VERS && count==0){
+		// in case of Version command -> handle here
+		CMD c;
+		c.cmd = CMD_VERS;
+		c.val1 = 0;
+		c.val2 = 1000;
+		send_CMD(c);
+		count = 0;
+	}
+	else{
+		packet.frame[offset + count] = in_key;
+		packet.frame[0] = offset + count;
+		count++;
+
+	}
+
 
 }
 
